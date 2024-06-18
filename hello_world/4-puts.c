a8909c27cb04187c79cb2aa3a7c947e5b94f05b1
S <vgozhyi@gmail.com>
2024-06-18 12:09:37 +1000

    Betty Fix 2
    E

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- main- Entry point
- Description: A C program that prints exactly
- "Programming is like buildinga multilingual puzzle
- using the puts() function
+main- Entry point
+Description: A C program that prints exactly
+"Programming is like buildinga multilingual puzzle
+using the puts() function
 
- Return: Always 0 (Success)
+Return: Always 0 (Success)
 */
 
 int main(void)
