c37718994a9ef128b6deff9118065d18ae7a5577
S <vgozhyi@gmail.com>
2024-06-21 18:20:27 +1000

    Testing teacher's version of assignment 1
    To see whether or not something it wrong.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,22 +8,24 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 	srand(time(0));
 	n = rand() - RAND_MAX /2;
+	lastDigit = (n % 10);
+	
+	printf("Last digit of %d is %d ", n, lastDigit);
 
-	printf("Last digit of %d is %d ", n, n % 10);
-
-	if(n > 5)
+	if(lastDigit >= 6)
 	{
 	  printf("and is greater than 5\n");
 	}
-	else if(n < 0)
-	{
-	  printf("and is less than 6 and not 0\n");
-	}
-	else if(n == 0)
+	else if(lastDigit == 0)
 	{
 	  printf("and is 0\n");
+	}
+	else
+	{
+	  printf("and is less than 6 and not 0\n");
 	}
 	return(0);
 }
