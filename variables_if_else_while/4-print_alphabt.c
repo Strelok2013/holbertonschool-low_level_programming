229736f9940c7a36a803901a42e4e804bbb2b237
Vlad <vgozhyi@gmail.com>
2024-06-22 23:51:02 +0000

    4th assignment
    Yea

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,17 +2,22 @@
 #include <time.h>
 #include <stdlib.h>
 
+/**
+ * main -  Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  int i = 0;
-  for (i = 97; i < 123; i++)
-    {
-      if(i != 101 && i != 113)
+	int i = 0;
+	for (i = 0; i < 123; i++)
 	{
-	  putchar(i);
+		if (i != 5 && i != 17)
+		{
+			putchar(i+ 97);
+		}
 	}
-    }
-  putchar(12);
-
-  return(0);
+	putchar(10);
+	return (0);
 }
