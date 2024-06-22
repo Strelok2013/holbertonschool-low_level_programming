a8c0ba4aadc92fb1394b2da46dc92add984b4124
S <vgozhyi@gmail.com>
2024-06-22 13:39:53 +1000

    Fixing and adding stuff
    Trying to fix the first assignment of the previous project and also starting the next one.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,30 +2,36 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
-	int lastDigit;
-	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	lastDigit = (n % 10);
-	
-	printf("Last digit of %d is %d ", n, lastDigit);
+	int last_digit;
 
-	if(lastDigit >= 6)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last_digit = (n % 10);
+	printf("Last digit of %d is %d ", n, last_digit);
+
+	if (last_digit >= 6)
 	{
-	  printf("and is greater than 5\n");
+		printf("and is greater than 5 \n");
 	}
-	else if(lastDigit == 0)
+	else
+	if(last_digit == 0)
 	{
-	  printf("and is 0\n");
+		printf("and is 0\n");
 	}
 	else
 	{
-	  printf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	}
+
 	return(0);
 }
