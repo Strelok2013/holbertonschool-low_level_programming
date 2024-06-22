5d27a30544a44cc5839de0a729f3071e1f3975da
Vlad <vgozhyi@gmail.com>
2024-06-22 10:19:50 +0000

    Testing on sandbox
    Rewrote the assignment code in the sandbox environment. Hopefully it passes the checker.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,29 @@
  *time it is executed.
  *Then, it prints whether or not the generated number is positive or negative.
  *Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	int n;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+  int n;
 
-	if(n > 0)
-		printf("is positive \n");
-	else if(n < 0)
-		printf("is negative \n");
-	else
-		printf("is zero \n");
-	return (0);
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
+  printf("%d ", n);
+  if(n > 0)
+    {
+      printf("is positive\n");
+    }
+  else
+    if(n == 0)
+    {
+      printf("is zero\n");
+    }
+  else
+    {
+      printf("is negative\n");
+    }
+  
+  return(0);
 }
