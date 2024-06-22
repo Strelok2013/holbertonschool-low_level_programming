8a70f066ab76ab35c20385f55c9ccdc2dd7b3aa7
Vlad <vgozhyi@gmail.com>
2024-06-22 10:50:51 +0000

    2nd question revision
    Working through it 'till Betty is happy.

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  int i;
-  for (i = 97; i < 123; i++)
-    {
-      putchar(i);
-    }
-  putchar(12);
-  return(0);
+	int i;
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar(12);
+	return (0);
 }
