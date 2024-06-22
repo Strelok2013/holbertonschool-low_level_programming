0ff5423125170aa9c65c8fa58741ca870a9735c4
Vlad <vgozhyi@gmail.com>
2024-06-22 23:46:11 +0000

    3rd
    assignment

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -14,12 +14,13 @@ int main(void)
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(i + 65);
+		putchar(i + 97);
 	}
 	for( i = 0; i < 26; i++)
 	{
-		putchar(i + 97);
+		putchar(i + 65);
 	}
+	putchar(10);
 	
 	return (0);
 }
