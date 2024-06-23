aca9fd3ef7e4a0eeb149caa2cba11093c3501f7a
Vlad <vgozhyi@gmail.com>
2024-06-23 01:21:47 +0000

    c
    d

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i = 0;
+	
 	for (i = 0; i < 26; i++)
 	{
 		putchar(122 - i);
