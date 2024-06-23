a647a1e151deb9dcf55f9d5de91c70b28b79963f
Vlad <vgozhyi@gmail.com>
2024-06-23 01:26:26 +0000

    8th
    assignment

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
 int main(void)
 {
-  int i = 0;
-  for(i = 0; i < 10; i++)
-    {
-      putchar(i + 48);
-    }
-  for(i = 0; i < 6; i++)
-    {
-      putchar(i + 97);
-    }
-  putchar(12);
-  return(0);
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + 97);
+	}
+
+	return (0);
 }
