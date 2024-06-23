b2e305e794047becbe61b933b0f27f9b4890ff2e
Vlad <vgozhyi@gmail.com>
2024-06-23 00:22:10 +0000

    D
    f

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,11 @@ int main(void)
 	int i = 0;
 	for (i = 0; i < 26; i++)
 	{
-		if (i != 5 && i != 17)
+		if ((i != 5) && (i != 17))
 		{
 			putchar(i + 97);
 		}
 	}
 	putchar(10);
-	return(0);
+	return (0);
 }
