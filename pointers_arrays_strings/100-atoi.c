192c19358c245ff54f064d45d2d0f82911cbd85b
Vlad <vgozhyi@gmail.com>
2024-06-29 11:02:58 +0000

    a
    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - generates an int from a given string
@@ -14,30 +15,29 @@ int _atoi(char *s)
 	int done = 0;
 	int sum = 0;
 
-	while (s[i] != '\0' && !done)
+	while (s[i] == ' ')
 	{
-		if (s[i] < ':' && s[i] > '/')
-		{
-			while (s[i] > '/' && s[i] < ':')
-			{
-				sum *= 10;
-				sum += (s[i] - '0');
-				i++;
-			}
-			done = 1;
-		}
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
 		i++;
 	}
-	if (sum != 0)
+	if (s[i] == '-' || s[i] == '+')
 	{
-		return (sum *= sign);
+		sign = 1 - 2 * (s[i++] == '-');
 	}
-	else
+
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		return (0);
+		if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+			{
+				return (INT_MAX);
+			}
+			else
+			{
+				return (INT_MIN);
+			}
+		}
+		sum = 10 * sum + (s[i++] - '0');
 	}
+	return (sum * sign);
 }
