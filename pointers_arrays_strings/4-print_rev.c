14756b8affed36948bad89b9a16c45668080dc44
Vlad <vgozhyi@gmail.com>
2024-06-29 09:04:05 +0000

    a
    a

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+61;7603;1c
 /**
  * print_rev - prints a given string in reverse
  * @s: pointer to char array or string
@@ -15,9 +15,10 @@ void print_rev(char *s)
 	{
 		i++;
 	}
-	while (i >= 0)
+	while ((i - 1) >= 0)
 	{
 		_putchar(s[i]);
 		i--;
 	}
+	_putchar('\n');
 }
