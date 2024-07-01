d2066a422194fc2e3e2420326c00183b6e073b62
Vlad <vgozhyi@gmail.com>
2024-07-01 01:43:04 +0000

    AA
    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,31 +12,25 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int sum = 0;
+	unsigned int sum = 0;
 
-	while (s[i] == ' ')
+	while (s[i] != '\0')
 	{
-		i++;
-	}
-	if (s[i] == '-' || s[i] == '+')
-	{
-		sign = 1 - 2 * (s[i++] == '-');
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && s[i] - '0' > 7))
+		if (s[i] < ':' && s[i] > '/')
 		{
-			if (sign == 1)
+			sum *= 10;
+			sum += (s[i] - '0');
+			if(s[i + 1] > ':' && s[i + 1] < '/')
 			{
-				return (INT_MAX);
-			}
-			else
-			{
-				return (INT_MIN);
+				break;
 			}
 		}
-		sum = 10 * sum + (s[i++] - '0');
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
 	}
+	
 	return (sum * sign);
 }
