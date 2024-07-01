f1360a72d42bc88b57045951b4a15f1de0a65558
Vlad <vgozhyi@gmail.com>
2024-07-01 04:38:49 +0000

    More Pointers, arrays and strings
    Next project, going to be submitted late thanks to the rigorous checking requirements.

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings into one
+ * @dest: the string to add onto
+ * @src: the string being added
+ *
+ * Return: char pointer, the resulting array dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] !='\0')
+	{
+		j++;
+	}
+	while (k <= j)
+	{
+		dest[i + k] = src[k];
+		k++;
+	}
+	return (dest);
+} 
---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k = 0;
+
+	while()
+	{
+		
+	}
+	while()
+	{
+		
+	}
+	while()
+	{
+		
+	}
+}
