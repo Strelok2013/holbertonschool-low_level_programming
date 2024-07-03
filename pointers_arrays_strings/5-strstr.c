843f82247a4ac8632035b6f731d01196f001b04c
Vlad <vgozhyi@gmail.com>
2024-07-03 11:47:27 +0000

    Quicksaving...
    Saving my work just in case.

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_strstr(char * haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	char *p = 0;
+
+	if(haystack[n] == needle[0])
+	{
+		// First matching char found
+		while (haystack[n] == needle[m])
+		{
+			// While the nth element matches mth element
+		}
+	}
+	
+}
