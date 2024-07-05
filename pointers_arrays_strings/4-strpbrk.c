02fb5430ef3a5256cdd0cf6fa3e0b7bf47dad0b9
Vlad <vgozhyi@gmail.com>
2024-07-05 00:41:41 +0000

    A
    a

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to char array, string to search
+ * @accept: pointer to char array, set of bytes to check against
  *
- *
- *
- *
- *
- *
+ * Return; pointer to char, first matching char in @s
  */
 
 char *_strpbrk(char *s, char *accept)
