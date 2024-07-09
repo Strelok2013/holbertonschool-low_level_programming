2a1c1d38d03a6f789f587ef79113abb849274515
Vlad <vgozhyi@gmail.com>
2024-07-09 05:31:26 +0000

    a
    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,34 +13,21 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0;
 	char *p;
 
-	if (s1)
-	{
-		while (s1[i] != '\0')
-			i++;
-	}
-	if (s2)
-	{
-		while (s2[j] != '\0')
-			j++;
-	}
-	if (i == 0 && j == 0)
-	{
-		return ("");
-	}
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[i] != '\0')
+		j++;
 	p = malloc(i + j);
 	if (!p)
 		return (0);
 	while (k < i)
-	{
 		p[k] = s1[k];
-		k++;
-	}
 	k = 0;
 	while (k < j)
-	{
 		p[i + k] = s2[k];
-		k++;
-	}
-	p[i + j] = '\0';
 	return (p);
 }
