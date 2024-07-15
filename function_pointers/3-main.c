f6bcab63a34b67fcccc65f1dfc1b13a7c4d3ac02
Vlad <vgozhyi@gmail.com>
2024-07-15 01:18:06 +0000

    Starting function pointers
    aaayea

---
+++ function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+Project folder for 'C - Function pointers'.
