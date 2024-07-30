95af42bd8a0ae27f276d4586696a6d5e3a2be701
S <vgozhyi@gmail.com>
2024-07-30 19:01:35 +1000

    A
    A

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 	}
 	if (!node->next)
-		return (add_dnodeint_end(h,n));
+		return (add_dnodeint_end(h, n));
 	newNode = malloc(sizeof(dlistint_t));
 	if (!newNode)
 		return (NULL);
