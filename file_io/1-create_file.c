7e858f155f2580b1412f04e6dc67d85d7628ebeb
Vlad <vgozhyi@gmail.com>
2024-08-05 21:21:49 +1000

    Rewrite of 3-cp
    This is the only major change other than the previous questions failing.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,15 +10,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fd, w;
+	int fd, w;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC);
-	if (fd == -1)
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (text_content)
-		w = write(fd, text_content, sizeof(text_content));
+	{
+		while(text_content[i])
+			i++;
+	}
+	w = write(fd, text_content, i);
 	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,52 +13,10 @@ int close_file(int fd)
 	c = close(fd);
 	if (c == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 	return (c);
-}
-
-/**
- * open_file - helper function to open a file
- * @filename: name of the file to open
- * @file: which file we're opening, 0 for source, 1 for dest
- *
- * Return: int, -1 on failure, anything else on success.
- */
-
-int open_file(char *filename, char file)
-{
-	int o = -1;
-
-	if (file == 0)
-	{
-		o = open(filename, O_RDONLY);
-		if (o == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", filename);
-			close_file(o);
-			exit (98);
-		}
-		return (o);
-	}
-	if (file == 1)
-	{
-		o = open(filename, O_WRONLY | O_TRUNC);
-		if (o == -1)
-		{
-			o = open(filename, O_WRONLY | O_CREAT, 0664);
-			if (o == -1)
-			{
-				dprintf(2, "Error: Can't write to %s\n", filename);
-				close_file(o);
-				exit(99);
-			}
-			return (o);
-		}
-		return (o);
-	}
-	return (o);
 }
 
 /**
@@ -72,22 +30,10 @@ int open_file(char *filename, char file)
  * Return: ssize_t, number of bytes written. -1 on failure
  */
 
-ssize_t read_write_from_buffer(int from, int to, void *buffer, ssize_t size)
+int read_write_from_buffer(int from, int to, void *buffer, ssize_t size)
 {
-	ssize_t r = 1, w;
+	int r = 1, w;
 
-	while (r)
-	{
-		r = read(from, buffer, size);
-		w = write(to, buffer, r);
-		if (w == -1 || r == -1)
-		{
-			close_file(from);
-			close_file(to);
-			return (-1);
-		}
-			
-	}
 	return (r);
 }
 
@@ -101,28 +47,21 @@ ssize_t read_write_from_buffer(int from, int to, void *buffer, ssize_t size)
 
 int main (int ac, char **av)
 {
-	ssize_t f_from, f_to, wr;
-	char *buffer = malloc(sizeof(char) * 1024);
-	
+	// Rewrite
+	int f_from, f_to;
+	char *buffer = malloc(sizeof(1024));
+
 	if (ac != 3)
 	{
-		dprintf(2, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
+		//first fail
 	}
 	if (!buffer)
 	{
-		dprintf(2, "%s\n", "Failed to create rd/wr buffer.\n");
-		exit (96);
+		// exit 99, can't write
 	}
-	f_from = open_file(av[1], 0);
-	f_to = open_file(av[2], 1);
-	wr = read_write_from_buffer(f_from, f_to, buffer, 1024);
-	if (wr == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	close_file(f_from);
-	close_file(f_to);
-	return (0);
+	f_from = open(av[1], O_RDONLY);
+	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	// while loop which reads and writes.
+	// checking to and from happens in read/write loop :P
+	
 }
