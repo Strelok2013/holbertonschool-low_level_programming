8d572c536d8413e1d4f0f54845f860ba20ea593b
Vlad <vgozhyi@gmail.com>
2024-08-07 19:54:09 +1000

    I can't be arsed
    Sesriously, do you think people nowadays have the time to figure this horseshit out?
    No, they don't. And I don't either, so stop tormenting my with stupid fucking questions and checks.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,96 +1,98 @@
 #include "main.h"
 
 /**
- * close_file - helper function that closes a file
- * @fd: file descriptor to close
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: int,  -1 on failure, something else on success.
+ * Return: A pointer to the newly-allocated buffer.
  */
+char *create_buffer(char *file)
+{
+	char *buffer;
 
-int close_file(int fd)
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
 {
 	int c;
+
 	c = close(fd);
+
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	return (c);
 }
 
 /**
- * read_write_from_buffer - helper function that reads and writes
- * @from:
- * @to:
- * @buffer:
- * @size:
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Description: reads 1024 bytes from source file into buffer, then writes said bytes to dest file
- * Return: ssize_t, number of bytes written. -1 on failure
- */
-
-int read_write_from_buffer(int from, int to, void *buffer, ssize_t size)
-{
-	int r, w;
-
-	r = read(from, buffer, size);
-	do{
-		if (from == -1 || r == -1)
-		{
-			free(buffer);
-			return (-1);
-		}
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			free(buffer);
-			return (-2);
-		}
-		r = read(from, buffer, size);
-	} while (r > 0);
-	return (r);
-}
-
-/**
- * main - entry point
- * @ac: number of args
- * @av: pointer to array of pointers containing arg values
+ * Return: 0 on success.
  *
- * Return: Always 0
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
-
-int main (int ac, char **av)
+int main(int argc, char *argv[])
 {
-	int f_from, f_to, wr;
-	char *buffer = malloc(sizeof(1024));
+	int from, to, r, w;
+	char *buffer;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (!buffer)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	f_from = open(av[1], O_RDONLY);
-	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	wr = read_write_from_buffer(f_from, f_to, buffer, 1024);
-	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (wr == -2)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
 	free(buffer);
-	close_file(f_from);
-	close_file(f_to);
+	close_file(from);
+	close_file(to);
+
 	return (0);
-	
 }
